224ef334fdc66342046d4ea0afc4745715cee24a
Young-Heredia <hse1010674@est.univalle.edu>
2024-10-06 21:52:38 -0400

    Version 1

--- server/index.js
+++ server/index.js
@@ -40,22 +40,58 @@ app.get('/api/redsalud', (req, res) => {
 });
 
 
+/* ****************************************************** */
+/* ************************ LOGIN *********************** */
+/* ****************************************************** */
+// LOGIN O AUTENTICACION DE USUARIOS CON VERIFICACION DE CORREO Y CONTRASEÑA
+app.get('/api/login', (req, res) => {
+  const { correo, contrasenia } = req.query;
+  
+  if (!correo || !contrasenia) {
+    return res.status(400).json({ error: 'Correo y contraseña son obligatorios' });
+  }
 
-/* ****************************************************** */
-/* ******************* PERSONAL MÉDICO ****************** */
-/* ****************************************************** */
-// PERSONAL MEDICO
-/*app.get('/api/medicos', (req, res) => {
-  const query = ` SELECT idPersona, CONCAT(primerNombre, ' ', IFNULL(segundoNombre,''), ' ', primerApellido, ' ', IFNULL(segundoApellido,'')) AS nombreCompleto, CI, correo, numeroCelular
-                  FROM persona
-                  WHERE rol = 'doctor';`;
-  db.query(query, (error, result) => {
+  const query = `SELECT *
+                 FROM persona
+                 WHERE estado = 1 AND (correo = ? AND contrasenia = ?);`;
+
+  db.query(query, [correo, contrasenia], (error, result) => {
     if (error) {
       return res.status(500).send(error);
     }
-    res.json(result);
+
+    if (result.length === 0) {
+      return res.status(401).json({ error: 'Credenciales incorrectas' });
+    }
+
+    res.json(result[0]);  // Envia el primer resultado de mi usuario
   });
-});*/
+});
+
+// RUTA PROTEGIDA SOLO PARA ADMINISTRADORES
+const verifyRole = (role) => {
+  return (req, res, next) => {
+    const { correo, contrasenia } = req.query;
+
+    const query = 'SELECT rol FROM persona WHERE correo = ? AND contrasenia = ?';
+    db.query(query, [correo, contrasenia], (error, result) => {
+      if (error || result.length ===0 || result[0].rol !== role) {
+        return res.status(403).json({ error: 'No autorizado' });
+      }
+      next();
+    });
+  };
+};
+
+app.get('/api/admin-data', verifyRole('administrador'), (req, res) => {
+  res.json({ message: 'Datos confidenciales del administrador' });
+});
+
+
+
+
+
+
 
 
 
